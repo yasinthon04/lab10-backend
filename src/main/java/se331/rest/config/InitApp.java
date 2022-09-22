46eeae7cca7abb7ba4be4929380bd1aa63f2b9f9
yasinthon04 <yasinthon_r@cmu.ac.th>
2022-09-22 22:53:38 +0700

    1.1-2.2

--- src/main/java/se331/rest/config/InitApp.java
+++ src/main/java/se331/rest/config/InitApp.java
@@ -22,7 +22,7 @@ public class InitApp implements ApplicationListener<ApplicationReadyEvent> {
                 .date("3rd Sept")
                 .time("3.00-4.00 pm.")
                 .petAllowed(false)
-                .organizer("CAMT").build());
+                .build());
         eventRepository.save(Event.builder()
                 .category("Academic")
                 .title("Commencement Day")
@@ -31,7 +31,7 @@ public class InitApp implements ApplicationListener<ApplicationReadyEvent> {
                 .date("21th Jan")
                 .time("8.00am-4.00 pm.")
                 .petAllowed(false)
-                .organizer("CMU").build());
+                .build());
         eventRepository.save(Event.builder()
                 .category("Cultural")
                 .title("Loy Krathong")
@@ -40,7 +40,7 @@ public class InitApp implements ApplicationListener<ApplicationReadyEvent> {
                 .date("21th Nov")
                 .time("8.00-10.00 pm.")
                 .petAllowed(false)
-                .organizer("Chiang Mai").build());
+                .build());
         eventRepository.save(Event.builder()
                 .category("Cultural")
                 .title("Songkran")
@@ -49,6 +49,6 @@ public class InitApp implements ApplicationListener<ApplicationReadyEvent> {
                 .date("13th April")
                 .time("10.00am - 6.00 pm.")
                 .petAllowed(true)
-                .organizer("Chiang Mai Municipality").build());
+                .build());
     }
 }
--- src/main/java/se331/rest/entity/Event.java
+++ src/main/java/se331/rest/entity/Event.java
@@ -3,10 +3,8 @@ package se331.rest.entity;
 
 import lombok.*;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
+import java.util.List;
 
 @Data
 @Builder
@@ -25,7 +23,11 @@ public class Event {
     String date;
     String time;
     Boolean petAllowed;
-    String organizer;
+    @ManyToOne
+    Organizer organizer;
+    @ManyToMany(mappedBy = "eventHistory")
+    List<Participant> participants;
+
 }
 
 
