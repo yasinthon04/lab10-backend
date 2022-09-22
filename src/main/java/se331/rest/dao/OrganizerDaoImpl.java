a1a319902649702ec7ba9ac8b9be445d3022eb53
yasinthon04 <yasinthon_r@cmu.ac.th>
2022-09-23 01:20:47 +0700

    6.5

--- src/main/java/se331/rest/dao/OrganizerDaoImpl.java
+++ src/main/java/se331/rest/dao/OrganizerDaoImpl.java
@@ -7,6 +7,8 @@ import org.springframework.stereotype.Repository;
 import se331.rest.entity.Organizer;
 import se331.rest.repository.OrganizerRepository;
 
+import java.util.Optional;
+
 @Repository
 public class OrganizerDaoImpl implements OrganizerDao{
     @Autowired
@@ -14,5 +16,9 @@ public class OrganizerDaoImpl implements OrganizerDao{
     @Override
     public Page<Organizer> getOrganizer(Pageable pageRequest){
         return organizerRepository.findAll(pageRequest);
+    }
+    @Override
+    public Optional<Organizer> findById(Long id){
+        return organizerRepository.findById(id);
     }
 }
--- src/main/java/se331/rest/service/EventServiceImpl.java
+++ src/main/java/se331/rest/service/EventServiceImpl.java
@@ -4,8 +4,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Service;
 import se331.rest.dao.EventDao;
+import se331.rest.dao.OrganizerDao;
 import se331.rest.entity.Event;
+import se331.rest.entity.Organizer;
 
+import javax.transaction.Transactional;
 import java.util.List;
 
 @Service
@@ -13,6 +16,8 @@ public class EventServiceImpl implements EventService {
     @Autowired
     EventDao eventDao;
 
+    @Autowired
+    OrganizerDao organizerDao;
     @Override
     public Integer getEventSize() {
         return eventDao.getEventSize();
@@ -29,7 +34,11 @@ public class EventServiceImpl implements EventService {
     }
 
     @Override
+        @Transactional
     public Event save(Event event) {
+        Organizer organizer = organizerDao.findById(event.getOrganizer().getId()).orElse(null);
+        event.setOrganizer(organizer);
+        organizer.getOwnEvents().add(event);
         return eventDao.save(event);
     }
 }
